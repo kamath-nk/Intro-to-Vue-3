cfaa36dfab004ee68c7b85171cd05bed9fc7b38c
= <=>
2022-07-23 08:22:23 +0530

    Final changes

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 const app = Vue.createApp({
-    data() {
-        return {
-            product: 'Socks'
-        }
-    }
-})
+  data() {
+    return {
+      product: "Socks",
+      image: "./assets/images/socks_blue.jpg",
+      link: "https://vuejs.org/guide/quick-start.html#with-build-tools",
+    };
+  },
+});
