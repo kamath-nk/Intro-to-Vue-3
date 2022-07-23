4ba7a7a1f5a51c123a31b65ec480177e9e5f39d3
= <=>
2022-07-23 18:47:12 +0530

    Changes from chapter

--- main.js
+++ main.js
@@ -1,9 +1,20 @@
 const app = Vue.createApp({
-    data() {
-        return {
-            cart: 0,
-            premium: true
-        }
+  data() {
+    return {
+      cart: [],
+      premium: true,
+    };
+  },
+  methods: {
+    updateCart(id) {
+      this.cart.push(id);
     },
-    methods: {}
-})
+    removeItem(id) {
+      const index = this.cart.indexOf(id);
+      if (index > -1) {
+        // only splice array when item is found
+        this.cart.splice(index, 1); // 2nd parameter means remove one item only
+      }
+    },
+  },
+});
